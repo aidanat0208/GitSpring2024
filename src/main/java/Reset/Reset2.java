0998e817063273dbfe6bf3f6c7f5ff07445d68e5
Aidana <aidana.tazhmamatova@gmail.com>
2024-08-14 16:29:48 +0600

    Reset 2

---
+++ src/main/java/Reset/Reset2.java
@@ -0,0 +1,5 @@
+package Reset;public class Reset2 {
+    public static void main(String[] args) {
+        System.out.println("This is reset2");
+    }
+}
---
+++ src/main/java/Reset/ResetMixed.java
@@ -0,0 +1,5 @@
+package Reset;
+
+public class ResetMixed {
+
+}
